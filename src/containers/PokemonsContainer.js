2c7fda79dc107a56ccbecec54291821ddeedd92e
Zhaoxian Hu <huzhx@uci.edu>
2021-02-25 22:06:43 -0800

    invoke the graphql query and load the pokemons to the Pokemon component

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import Pokemon from '../components/Pokemon';
+import GET_POKEMONS from '../graphql/get-pokemons';
 
 const PokemonsContainer = () => {
-  return <p>I am a pokemon container</p>;
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
+  return (
+    <div className="container">
+      {pokemons && pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+    </div>
+  );
 };
 
 export default PokemonsContainer;

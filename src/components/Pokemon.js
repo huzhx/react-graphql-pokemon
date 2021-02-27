189a5c4f93c3b94710720717fc500175fa7711ed
Zhaoxian Hu <huzhx@uci.edu>
2021-02-26 18:20:46 -0800

    render attacks

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -13,7 +13,11 @@ const Pokemon = ({ pokemon }) => {
       <div className="pokemon__image">
         <img src={pokemon.image} alt={pokemon.name} />
       </div>
-      <div className="pokemon__attacks"></div>
+      <div className="pokemon__attacks">
+        {pokemon.attacks.special.slice(0, 3).map((attack) => (
+          <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+        ))}
+      </div>
     </div>
   );
 };
